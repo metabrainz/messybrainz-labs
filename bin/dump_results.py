15a6ad902f08d58dfc8f7e1406f5beef1571fb48
Robert Kaye <rob@musicbrainz.org>
2019-09-16 15:56:39 +0000

    Rework data output to be arbitrary levels

--- bin/dump_results.py
+++ bin/dump_results.py
@@ -6,6 +6,9 @@ import os
 import ujson
 import re
 from operator import itemgetter
+from copy import deepcopy
+
+NUM_LEVELS = 3
 
 def dump_artists_to_html():
 
@@ -62,7 +65,6 @@ def dump_artists_to_html():
 
 def dump_recordings_to_html():
 
-    total_count = 0
 # MSB
 #    SELECT lower(musicbrainz.musicbrainz_unaccent(rj.data->>'artist'::TEXT)) AS artist_name, artist as artist_msid,
 #           lower(musicbrainz.musicbrainz_unaccent(rj.data->>'title'::TEXT)) AS recording_name, gid AS recording_msid
@@ -71,6 +73,8 @@ def dump_recordings_to_html():
 #    SELECT DISTINCT lower(musicbrainz.musicbrainz_unaccent(artist_credit_name)) as artist_credit_name, artist_mbids, 
 #           lower(musicbrainz.musicbrainz_unaccent(recording_name)) AS recording_name, recording_mbid
 #           lower(musicbrainz.musicbrainz_unaccent(release_name)) AS release_name, release_mbid
+
+    total_count = 0
 
     print("load & categorize recordings")
     categories = {}
@@ -82,29 +86,22 @@ def dump_recordings_to_html():
 
             total_count += 1
             data = ujson.loads(line)
-            k = data[3]
-            if re.search("^\w\w", k, flags=re.A):
-                k0 = data[3][0]
-                k1 = data[3][1]
-            elif re.search("^\W\w", k, flags=re.A):
-                k0 = "*"
-                k1 = data[3][1]
-            elif re.search("^\w\W", k, flags=re.A):
-                k0 = data[3][0]
-                k1 = "*"
-            elif re.search("^\W\W", k, flags=re.A):
-                k0 = "*"
-                k1 = "*"
 
-            while True:
-                try:
-                    categories[k0][k1].append(data)
-                    break
-                except KeyError:
-                    if not k0 in categories:
-                        categories[k0] = {}
-                    if not k1 in categories[k0]:
-                        categories[k0][k1] = []
+            key = data[3]
+            category = ""
+
+            for i in range(NUM_LEVELS):
+                ch = key[i : i+1]
+                if re.search("^\w", ch, flags=re.A):
+                    category += ch
+                else:
+                    category += '*'
+
+            try:
+                categories[category].append(data)
+            except KeyError:
+                categories[category] = [data]
+
 
 
     print("output recordings")
@@ -113,47 +110,94 @@ def dump_recordings_to_html():
     except FileExistsError:
         pass
 
-    for top_cat in categories.keys():
-        for bot_cat in categories[top_cat].keys():
-            with open("html/recording/%s%s.html" % (top_cat, bot_cat), "w") as f:
-                cat = top_cat + bot_cat
-                f.write('<html><head><meta charset="UTF-8"><title>%s recordings</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css"></head><body>\n' % cat)
-                f.write("<h1>%s recordings</h1>" % cat)
-                f.write("<p>count: %d</p>" % len(categories[top_cat][bot_cat]))
-                f.write("<table><tr><th>MsB recording</th><th>MsB artist</th><th>MB artist</th>")
-                f.write("<th>MB recording</th><th>MB release</th><th>rec id</th><th>rel id</th></tr>\n")
+    for cat in sorted(categories.keys()):
+        print("%s" % cat, end="\b\b\b\b")
+        sys.stdout.flush()
+        dest_dir = os.path.join("html", "recording", *(list(cat[0:-1]))) 
+        try:
+            os.makedirs(dest_dir)
+        except FileExistsError:
+            pass
 
-                for count, msb_artist_name, msb_artist_id, msb_recording_name, msb_recording_id, msb_release_name, msb_release_id, \
-                    mb_artist_name, mb_artist_ids, mb_recording_name, mb_recording_id, mb_release_name, mb_release_id in sorted(categories[top_cat][bot_cat], key=itemgetter(3,5,7)):
+        with open(os.path.join(dest_dir, "%s.html" % cat[-1:]), "w") as f:
+            f.write('<html><head><meta charset="UTF-8"><title>%s recordings</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css"></head><body>\n' % cat)
+            f.write("<h1>%s recordings</h1>" % cat)
+            f.write("<p>count: %d</p>" % len(categories[cat]))
+            f.write("<table><tr><th>MsB recording</th><th>MsB artist</th><th>MB artist</th>")
+            f.write("<th>MB recording</th><th>MB release</th><th>rec id</th><th>rel id</th></tr>\n")
 
-                    f.write('<tr>')
-                    f.write('<td title="%s">%s</td>' % (msb_recording_id, msb_recording_name))
-                    f.write('<td title="%s">%s</td>' % (msb_artist_id, msb_artist_name))
-                    f.write('<td>')
-                    f.write('<a title="%s" href="https://musicbrainz.org/artist/%s">%s</a> ' % (mb_artist_ids[0], mb_artist_ids[0], mb_artist_name))
-                    for i, mbid in enumerate(mb_artist_ids[1:]):
-                        f.write('<a title="%s" href="https://musicbrainz.org/artist/%s">%s</a> ' % ((mbid, mbid, "id #%d" % i)))
+            last_data = []
+            for data in sorted(categories[cat], key=itemgetter(3,5,7)):
 
-                    f.write('</td>')
-                    f.write('<td><a title="%s" href="https://musicbrainz.org/recording/%s">%s</a></td>' % (mb_recording_id, mb_recording_id, mb_recording_name))
-                    f.write('<td><a title="%s" href="https://musicbrainz.org/release/%s">%s</a></td>' % (mb_release_id, mb_release_id, mb_release_name))
-                    f.write('<td title="%s">%s</td>' % (mb_recording_id, mb_recording_id[0:6]))
-                    f.write('<td title="%s">%s</td>' % (mb_release_id, mb_release_id[0:6]))
-                    f.write('</tr>\n')
-                f.write("</table></body></html>\n")
+                # If rows are the same as the previous row, skip it
+                if last_data and data[1] == last_data[1] and data[3] == last_data[3] and data[5] == last_data[5] and data[7] == last_data[7] and data[8] == last_data[8] and \
+                    data[9] == last_data[9] and data[10] == last_data[10] and data[11] == last_data[11] and data[12] == last_data[12]:
+                    print("skip")
+                    continue
+
+                count, msb_artist_name, msb_artist_id, msb_recording_name, msb_recording_id, msb_release_name, msb_release_id, \
+                    mb_artist_name, mb_artist_ids, mb_recording_name, mb_recording_id, mb_release_name, mb_release_id = data
+
+                last_data = data
+
+                f.write('<tr>')
+                f.write('<td title="%s">%s</td>' % (msb_recording_id, msb_recording_name))
+                f.write('<td title="%s">%s</td>' % (msb_artist_id, msb_artist_name))
+                f.write('<td>')
+                f.write('<a title="%s" href="https://musicbrainz.org/artist/%s">%s</a> ' % (mb_artist_ids[0], mb_artist_ids[0], mb_artist_name))
+                for i, mbid in enumerate(mb_artist_ids[1:]):
+                    f.write('<a title="%s" href="https://musicbrainz.org/artist/%s">%s</a> ' % ((mbid, mbid, "id #%d" % i)))
+
+                f.write('</td>')
+                f.write('<td><a title="%s" href="https://musicbrainz.org/recording/%s">%s</a></td>' % (mb_recording_id, mb_recording_id, mb_recording_name))
+                f.write('<td><a title="%s" href="https://musicbrainz.org/release/%s">%s</a></td>' % (mb_release_id, mb_release_id, mb_release_name))
+                f.write('<td title="%s">%s</td>' % (mb_recording_id, mb_recording_id[0:6]))
+                f.write('<td title="%s">%s</td>' % (mb_release_id, mb_release_id[0:6]))
+                f.write('</tr>\n')
+            f.write("</table></body></html>\n")
+
+    print("write indexes")
+    write_indexes("", categories, os.path.join("html", "recording"))
+    print("done")
 
 
-    with open("html/recording/index.html", "w") as f:
-        f.write('<html><head><meta charset="UTF-8"><title>artist matches</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css"></link></head><body>\n')
-        f.write("<h1>recording matches</h1>")
-        for top_cat in sorted(categories.keys()):
-            f.write('<h3>%s</h3><p>' % top_cat.upper())
-            for bot_cat in sorted(categories[top_cat].keys()):
-                cat = top_cat + bot_cat
-                f.write('<a href="%s.html">%s</a> ' % (cat, cat.upper()))
-            f.write('</p>')
+def write_indexes(level, categories, dest_dir):
+
+    if len(level) == NUM_LEVELS - 1:
+        return
+
+    items = []
+    for ch in list("abcdefghijklmnopqrstuvwxyz0123456789_*"):
+        new_level = level + ch
+        for cat in categories.keys():
+            if cat.startswith(new_level):
+                items.append(ch)
+                break
+
+    if level == "":
+        file_name = "index.html"
+        title = "Recording matches: Main index"
+    else:
+        file_name = "index-%s.html" % level
+        title = "Recording matches: index %s" % level.upper()
+
+    with open(os.path.join(dest_dir, file_name), "w") as f:
+        f.write('<html><head><meta charset="UTF-8"><title>%s</title><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/kognise/water.css@latest/dist/light.min.css"></link></head><body>\n' % title)
+        f.write("<h1>%s</h1>" % title)
+        if len(level):
+            f.write('<p><a href="/html/recording/index.html">top index</a></p>')
+
+        for item in items:
+            full_item = level + item
+            if len(level) == NUM_LEVELS - 1:
+                f.write('<p><a href="%s.html">%s</a></p>' % (os.path.join(*list(full_item)), full_item.upper()))
+            else:
+                f.write('<p><a href="index-%s.html">%s</a></p>' % (full_item, full_item.upper()))
             
-        f.write("<p></body></html>\n")
+        f.write("</body></html>\n")
+
+    for ch in list("abcdefghijklmnopqrstuvwxyz0123456789_*"):
+        write_indexes(level + ch, categories, dest_dir)
 
 
 if __name__ == "__main__":
