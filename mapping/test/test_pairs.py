988d1f5a5401189e003e75c67ba28cba219ffb1a
Robert Kaye <rob@musicbrainz.org>
2020-06-03 00:15:00 +0200

    clickify the run.py program

--- mapping/test/test_pairs.py
+++ mapping/test/test_pairs.py
@@ -4,59 +4,8 @@ import psycopg2
 
 import config
 
-# TODO: Make this work for punct removed data as well
-
-TEST_PAIRS_QUERY = '''
-    SELECT m.recording_id
-      FROM mapping.recording_artist_credit_pairs m
-     WHERE artist_credit_name = %s 
-       AND recording_name = %s
-'''
-
-def _read_test_data(filename):
-    data = []
-    with open(filename, newline='') as csvfile:
-         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-         for row in reader:
-             if not row:
-                if config.USE_MINIMAL_DATASET:
-                    break
-                else:
-                    continue
-
-             data.append(row)
-
-    return data
-
+# NOT IMPLEMENTED
 
 def test_pairs():
+    pass
 
-    data = _read_test_data("mapping/test/mapping_test_cases.csv")
-
-    passed = 0
-    failed = 0
-
-    with psycopg2.connect(config.DB_CONNECT_MB) as conn:
-        with conn.cursor() as curs:
-            for rdata in data:
-                curs.execute(TEST_PAIRS_QUERY, (rdata[1], rdata[0]))
-                row = curs.fetchone()
-                if row:
-                    passed += 1
-                else:
-                    failed += 1
-                    print("no match for '%s' '%s'" % (rdata[0], rdata[1]))
-
-                if row[0] != int(rdata[2]):
-                    mbid = get_mbid_for_release_id(mb_curs, int(row[0]))
-                    print("'%s' '%s' expected %s, got %s (%s)" % (rdata[0], rdata[1], rdata[2], row[0], mbid))
-                    failed += 1
-                else:
-                    print("'%s' '%s' ok" % (rdata[0], rdata[1]))
-                    passed += 1
-
-    print("%d passed, %d failed." % (passed, failed))
-    if failed == 0:
-        sys.exit(0)
-    else:
-        sys.exit(-1)
--- run.py
+++ run.py
@@ -2,19 +2,32 @@
 
 import sys
 
+import click
+
 from mapping.msid_mapping import create_mapping
 from mapping.recording_pairs import create_pairs
 from mapping.test.test_mapping import test_mapping
 from mapping.test.test_pairs import test_pairs
 
-prog = sys.argv[1]
 
-if prog == 'create-mapping':
-    create_mapping()
-elif prog == 'create-pairs':
-    create_pairs() 
-elif prog == 'test':
-#    test_pairs() 
-    test_mapping() 
-else:
-    print("huh?")
+@click.command()
+@click.argument("action", nargs=1)
+def mapping(action):
+    if action == 'create-mapping':
+        create_mapping()
+    elif action == 'create-pairs':
+        create_pairs() 
+    elif action == 'test':
+        test_mapping() 
+    else:
+        print("unknown action: %s" % action)
+
+
+def usage(command):
+    with click.Context(command) as ctx:
+        click.echo(command.get_help(ctx))
+
+
+if __name__ == "__main__":
+    mapping()
+    sys.exit(0)
